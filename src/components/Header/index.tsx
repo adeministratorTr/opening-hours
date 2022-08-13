33766487148a3d66421955064e9cce795a71693b
adeministratorTr <adem.bavas@healthhero.com>
2022-08-13 12:07:50 +0200

    Add eslint and prettier & Format

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
 import './style.css';
 
 export default function Header() {
-	return (
-		<header className="App-header">
-			<p>
-				Welcome to Opening Hour Project
-			</p>
-		</header>
-	);
+  return (
+    <header className="App-header">
+      <p>Welcome to Opening Hour Project</p>
+    </header>
+  );
 }
