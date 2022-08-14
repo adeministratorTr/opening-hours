81f4c97939d6e47a10470588bdcc898b3d9b4ad0
adeministratorTr <adem.bavas@healthhero.com>
2022-08-14 20:12:03 +0200

    Update DayTimeList component & style

--- src/components/DayTimeList/index.tsx
+++ src/components/DayTimeList/index.tsx
@@ -1,17 +1,19 @@
 import Divider from 'components/Divider';
 
+import styles from './DayTimeList.module.scss';
+
 export default function DayTimeList({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
+    <div className={styles.container}>
       {/* TODO get title as prop */}
-      <div>Opening hours</div>
+      <div className={styles.titleContainer}>
+        <img
+          src="https://png.pngitem.com/pimgs/s/10-101450_circle-hd-png-download.png"
+          alt="clock-icon"
+          height={15}
+        />
+        <div className={styles.title}>Opening hours</div>
+      </div>
       <div style={{ width: '250px' }}>
         <Divider />
       </div>
