c2a7b16e1af3c7ffc5595f4b838906c9f07495b8
adeministratorTr <adem.bavas@healthhero.com>
2022-08-14 21:26:21 +0200

    Add title prop to DayTimeList component

--- src/components/DayTimeList/index.tsx
+++ src/components/DayTimeList/index.tsx
@@ -2,22 +2,23 @@ import Divider from 'components/Divider';
 
 import styles from './DayTimeList.module.scss';
 
-export default function DayTimeList({ children }: { children: React.ReactNode }) {
+type TDayTimeListProps = {
+  title?: string;
+  children: React.ReactNode;
+};
+export default function DayTimeList({ title, children }: TDayTimeListProps) {
   return (
-    <div className={styles.container}>
-      {/* TODO get title as prop */}
+    <div className={styles.container} data-testid="DayTimeList">
       <div className={styles.titleContainer}>
         <img
           src="https://png.pngitem.com/pimgs/s/10-101450_circle-hd-png-download.png"
           alt="clock-icon"
           height={15}
         />
-        <div className={styles.title}>Opening hours</div>
+        <div className={styles.title}>{title || 'Opening hours'}</div>
       </div>
-      <div style={{ width: '250px' }}>
-        <Divider />
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>{children}</div>
+      <Divider />
+      <div data-testid="DayTimeListChildren">{children}</div>
     </div>
   );
 }
