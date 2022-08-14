5974d6885704ac1500b042ef37c89ac3e8ae633a
adeministratorTr <adem.bavas@healthhero.com>
2022-08-14 20:44:26 +0200

    Fix Header test name

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 
 import Header from './index';
-describe('Divider', () => {
+
+describe('Header', () => {
   it('should render default', () => {
     render(<Header />);
 
