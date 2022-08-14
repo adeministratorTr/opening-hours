154724a795a85ecd5637a5864dc444ebcac3ca2e
adeministratorTr <adem.bavas@healthhero.com>
2022-08-14 12:25:14 +0200

    Add basic data render in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from 'react';
+import { getRestaurantTimeSlots } from 'services/restaurant';
+import { mapDateTime, TMappedDateTime } from './mapper';
+
 export default function Home() {
-  return <p>Home Page</p>;
+  const [days, setDays] = useState<TMappedDateTime[]>();
+  useEffect(() => {
+    setDays(mapDateTime(getRestaurantTimeSlots()));
+  }, []);
+
+  function renderNoResult() {
+    return <p>Ooops. This place is closed!</p>;
+  }
+
+  return (
+    <div>
+      {!days || (days === null && renderNoResult())}
+      {days && days.length === 0 && renderNoResult()}
+      {days && days.length > 1 && (
+        <>
+          <div>Opening Hours</div>
+          <p>----------------------------</p>
+          {days.map((item, index) => (
+            <p key={index}>
+              {item.day}: {item.value}
+            </p>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
---
+++ src/pages/Home/mapper.ts
@@ -0,0 +1,67 @@
+import { TRestaurantDays, Days, Status } from 'services/types';
+
+export type TMappedDateTime = {
+  //@TODO move to constant file
+  day: Days;
+  value: string;
+};
+
+// Main motivation of this file/function is doing all mapping actions here
+// and try to keep page as simple as possible. If pages includes just business logic,
+// it'll be easier to add new feature/debug/change current state.
+
+export function mapDateTime(fullData: TRestaurantDays): TMappedDateTime[] {
+  const mappedObject: TMappedDateTime[] = [
+    {
+      day: Days.Monday,
+      value: getBusinessTimeText(fullData[Days.Monday])
+    },
+    {
+      day: Days.Tuesday,
+      value: getBusinessTimeText(fullData[Days.Tuesday])
+    },
+    {
+      day: Days.Wednesday,
+      value: getBusinessTimeText(fullData[Days.Wednesday])
+    },
+    {
+      day: Days.Thursday,
+      value: getBusinessTimeText(fullData[Days.Thursday])
+    },
+    {
+      day: Days.Friday,
+      value: getBusinessTimeText(fullData[Days.Friday])
+    },
+    {
+      day: Days.Saturday,
+      value: getBusinessTimeText(fullData[Days.Saturday])
+    },
+    {
+      day: Days.Sunday,
+      value: getBusinessTimeText(fullData[Days.Sunday])
+    }
+  ];
+
+  return mappedObject;
+}
+
+// @TODO handle day item "close-open-close" case
+// Now its working only in happy path
+
+function getBusinessTimeText(dayList: any[]): string | any {
+  if (!dayList || dayList === null) return Status.Close;
+  if (dayList.length === 0) return Status.Close; // if certain day has no data, that means shop closed that day.
+  if (dayList.length % 2 === 0) {
+    return getTextFromSecond({ second: dayList[0].value }) + ' - ' + getTextFromSecond({ second: dayList[1].value });
+  }
+
+  // Just to be safe, I decided to assign closed text. Discussable, Changeable according to needs
+  return Status.Close;
+}
+
+function getTextFromSecond({ second }: { second: number }): string {
+  const time = new Date(0);
+  time.setSeconds(second);
+  const hour = time.toLocaleString('en-US', { timeZone: 'UTC', hour: 'numeric', hour12: true });
+  return hour;
+}
