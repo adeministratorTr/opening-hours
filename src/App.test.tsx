bddbd0d8cdf9aaefff79e7f4eda3ae00ac28a308
adeministratorTr <adem.bavas@healthhero.com>
2022-08-15 10:46:19 +0200

    Refactor App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders default', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Opening Hour Project/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render default', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Opening Hour Project/i);
+    expect(linkElement).toBeVisible();
+  });
 });
