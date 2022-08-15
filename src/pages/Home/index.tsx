1f16019a12890b92c47123b36ee134dc7205e2bc
adeministratorTr <adem.bavas@healthhero.com>
2022-08-15 11:37:48 +0200

    Add TODAY badge

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,12 +7,18 @@ import Divider from 'components/Divider';
 import { mapDateTime, TMappedDateTime } from './mapper';
 
 import styles from './Home.module.scss';
+import { Days } from 'services/types';
 
 export default function Home() {
   const [days, setDays] = useState<TMappedDateTime[]>();
   useEffect(() => {
     setDays(mapDateTime(getRestaurantTimeSlots()));
   }, []);
+
+  function showTodayBadge({ day }: { day: Days }): boolean {
+    const today = new Date().toLocaleString('en-us', { weekday: 'long' });
+    return today.toLowerCase() === day.toLowerCase();
+  }
 
   function renderNoResult() {
     return <p>Ooops. This place is closed!</p>;
@@ -22,16 +28,20 @@ export default function Home() {
     <div>
       {!days || (days === null && renderNoResult())}
       {days && days.length === 0 && renderNoResult()}
-      {/* @TODO Add "TODAY" badge */}
       {days && days.length > 1 && (
         <div className={styles.dayTimeWrapper}>
           <DayTimeList>
             {days.map((item, index) => (
               <React.Fragment key={index}>
                 <div className={styles.dayTimeItem}>
-                  <p>{item.day}</p>
+                  <div>
+                    {item.day}{' '}
+                    {showTodayBadge({ day: item.day }) && (
+                      <div className={styles.badge}>TODAY</div>
+                    )}
+                  </div>
                   <p>{item.value}</p>
-                  {/* Add gray color to "Closed" */}
+                  {/* Add gray color to "Close" */}
                 </div>
                 <Divider variant="light" />
               </React.Fragment>
